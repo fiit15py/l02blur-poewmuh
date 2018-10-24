aae4ab049134cdbe1a2b7b8e52ea0ded467051a9
Студент <grandow78@gmail.com>
2018-10-24 17:24:49 +0900

    gaussian_blur v0

--- gaussian_blur.py
+++ gaussian_blur.py
@@ -4,7 +4,25 @@ from PIL import Image
 from math import pi, log, exp
 import numpy as np
 import sys
-
+def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█'):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+        fill        - Optional  : bar fill character (Str)
+    """
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix), end = '\r')
+    # Print New Line on Complete
+    if iteration == total: 
+        print()
 def process(filename, r):
     # должна обрабатывать файл filename гауссовым размытием в квадрате [-r, +r] x [-r, +r] 
     # и записывать результат в <filename>.gaussblurred.png
@@ -15,8 +33,23 @@ def process(filename, r):
     sigma = 0.38*r
     gauss_dist = np.exp( -(dx*dx+dy*dy)/(2*sigma**2) ) / (2*pi*sigma**2)
     coeff = gauss_dist / np.sum(gauss_dist)
+    w = 3000
+    h = 3667
+    A = np.array(img.getdata())
+    A = A.reshape(w,h)
+    B = np.zeros((w,h))
+    B = B.reshape(h,r)
+    full = w*h
+    percent = 0
+    for i in range(r,w-r-1):
+        for j in range(r,h-r-1):
+            for k in range(-r,r+1):
+                for m in range(-r,r+1):
+                    B[i,j] += coeff[k+r,m+r] * A[i+k,j+m]
+                    percent +=1
+                    printProgressBar(percent,full,prefix = 'Progress:', suffix = 'Complete',length = 1)
+        
 
-    # код сюда ....
     newimg = img
     newimg.show()
     newimg.save(filename+'.gaussblurred.png')
@@ -26,6 +59,6 @@ def process(filename, r):
 if __name__=='__main__':
     # Запускать с командной строки с аргументом <имя файла>, например: python gauss.py darwin.png
     if len(sys.argv) > 1:
-        main(sys.argv[1], 3)
+        process(sys.argv[1], 3)
     else:
         print("Must give filename.\n")
