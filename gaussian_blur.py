5f4f3cdac13e6ffc1643d15a5fa0e07e64834aea
poewmuh <grandow78@gmail.com>
2018-10-30 12:03:45 +0900

    na pare.(not working)

--- gaussian_blur.py
+++ gaussian_blur.py
@@ -1,34 +1,17 @@
-# -*- coding: utf-8 -*-
-
 from PIL import Image
 from math import pi, log, exp
 import numpy as np
 import sys
 def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█'):
-    """
-    Call in a loop to create terminal progress bar
-    @params:
-        iteration   - Required  : current iteration (Int)
-        total       - Required  : total iterations (Int)
-        prefix      - Optional  : prefix string (Str)
-        suffix      - Optional  : suffix string (Str)
-        decimals    - Optional  : positive number of decimals in percent complete (Int)
-        length      - Optional  : character length of bar (Int)
-        fill        - Optional  : bar fill character (Str)
-    """
     percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
     filledLength = int(length * iteration // total)
     bar = fill * filledLength + '-' * (length - filledLength)
     print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix), end = '\r')
-    # Print New Line on Complete
     if iteration == total: 
         print()
 def process(filename, r):
-    # должна обрабатывать файл filename гауссовым размытием в квадрате [-r, +r] x [-r, +r] 
-    # и записывать результат в <filename>.gaussblurred.png
     img = Image.open(filename)
     img.load()
-
     dx, dy = np.meshgrid(np.arange(-r, +r+1, 1.), np.arange(-r, +r+1, 1.0))
     sigma = 0.38*r
     gauss_dist = np.exp( -(dx*dx+dy*dy)/(2*sigma**2) ) / (2*pi*sigma**2)
@@ -38,7 +21,6 @@ def process(filename, r):
     A = np.array(img.getdata())
     A = A.reshape(w,h)
     B = np.zeros((w,h))
-    B = B.reshape(h,r)
     full = w*h
     percent = 0
     for i in range(r,w-r-1):
@@ -46,11 +28,11 @@ def process(filename, r):
             for k in range(-r,r+1):
                 for m in range(-r,r+1):
                     B[i,j] += coeff[k+r,m+r] * A[i+k,j+m]
-                    percent +=1
-                    printProgressBar(percent,full,prefix = 'Progress:', suffix = 'Complete',length = 1)
+            percent +=1
+            printProgressBar(percent,full,prefix = 'Progress:', suffix = 'Complete',length = 50)
         
 
-    newimg = img
+    newimg = Image.fromarray(B)
     newimg.show()
     newimg.save(filename+'.gaussblurred.png')
 
